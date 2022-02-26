514d6c838b4acac21bae2636d6160e7bcf4affe6
Michael Veit <mikeveit1@gmail.com>
2022-02-26 13:46:34 -0500

    wrote math program

---
+++ add.py
@@ -0,0 +1,8 @@
+num_1 = int(input("Please enter the first number: "))
+num_2 = int(input("Please enter the second number: "))
+num_sum = num_1 + num_2
+print("The sum is", num_sum)
+
+# if you wanted to get really specific you could say:
+# print("The sum of", num_1, "and", num_2, "is", num_sum)
+# uncomment the line above and see for yourself!
